6f8812b413132d2ed72e16220b589f92a83b5cb1
Terrell-Alves <terrell.alves@mscoding.org>
2025-06-03 13:55:22 -0500

    javascript changes
    Made each individual button have a response

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,20 @@
-let input;
-let operation;
-let input2;
 let buttons = document.querySelectorAll('.calcButton');
 let result = document.querySelector('#result');
 
-function displayValue () {
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+    console.log(result);
+    result.value += buttons[i].innerText;
+})
 }
+
+// Save input number to variable 
+function inputValue () {
+    let input = result.value;
+}
+
+buttons[0].addEventListener('click', () => {
+    result.value = '';
+});
+
+buttons[1].addEventListener('click', inputValue);
